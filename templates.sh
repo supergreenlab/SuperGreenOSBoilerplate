c93ef4c8711ec87adf5ee6ab0a23532fe452a418
vitaminwater <stantin04@gmail.com>
2019-01-08 22:34:26 +0100

    template.sh aesthetics and optimization

--- templates.sh
+++ templates.sh
@@ -21,41 +21,42 @@ if [ "$#" -ne 2 ]; then
   exit
 fi
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m"
+
 TEMPLATE_NAME="$1"
 MODULE_NAME="$2"
 
 mkdir -p main/$MODULE_NAME
-echo "Copying files to main/$MODULE_NAME"
-echo "$(sed "s/CAPS_NAME/${MODULE_NAME^^}/g;s/NAME/$MODULE_NAME/g"  templates/$TEMPLATE_NAME.yml)" > /tmp/$TEMPLATE_NAME
+echo -e "Copying files to main/$MODULE_NAME: ${GREEN}Done${NC}"
+echo -e "$(sed "s/CAPS_NAME/${MODULE_NAME^^}/g;s/NAME/$MODULE_NAME/g"  templates/$TEMPLATE_NAME.yml)" > /tmp/$TEMPLATE_NAME
 for i in $(find templates/$TEMPLATE_NAME/*)
 do
   FILE="$(basename ${i/.template/})"
   FILE_PATH="main/$MODULE_NAME/${FILE/$TEMPLATE_NAME/$MODULE_NAME}"
   if [[ "$i" == *".template" ]]; then
-    echo "Call mustache for $i to $FILE_PATH"
     mustache /tmp/$TEMPLATE_NAME $i > "$FILE_PATH"
+    echo -e "Call mustache for $i to $FILE_PATH: ${GREEN}Done${NC}"
   else
-    echo "Copying file $i to $FILE_PATH"
     cp $i $FILE_PATH
+    echo -e "Copying file $i to $FILE_PATH: ${GREEN}Done${NC}"
   fi
 done
 
 
 if ! grep -q -- "- $MODULE_NAME" config.yml; then
-  if [ "$TEMPLATE_NAME" == "new_module" ]; then
-    echo "Adding module to custom_modules in config.yml"
-    sed -i "/custom_modules:/ a - $MODULE_NAME" config.yml
-  elif [ "$TEMPLATE_NAME" == "new_i2c_device" ]; then
-    echo "Adding i2c device to i2c_devices in config.yml"
-    sed -i "/i2c_devices:/ a - $MODULE_NAME" config.yml
-  fi
-fi
 
-GREEN="\033[0;32m"
-RED="\033[0;31m"
-NC="\033[0m"
-echo "==="
-echo -e "${GREEN}Done${NC}"
-echo "==="
-echo "Running ./update_template.sh...."
-./update_template.sh
+  if [ "$TEMPLATE_NAME" == "new_module" ]; then
+    sed -i "/custom_modules:/ a \  - $MODULE_NAME" config.yml
+    echo -e "Adding module to custom_modules in config.yml: ${GREEN}Done${NC}"
+  elif [ "$TEMPLATE_NAME" == "new_i2c_device" ]; then
+    sed -i "/i2c_devices:/ a \  - $MODULE_NAME" config.yml
+    echo -e "Adding i2c device to i2c_devices in config.yml: ${GREEN}Done${NC}"
+  fi
+
+  echo "==="
+  echo "Running ./update_template.sh...."
+  ./update_template.sh
+
+fi
